bc57c017f7c51cde9cb64d032616057888dad88f
Oleksii Hiriavenko <martmarchmartmarch@gmail.com>
2023-06-06 22:53:04 +0300

    gh-pages version

--- src/js/app.js
+++
@@ -1,8 +0,0 @@
-import * as header from "./modules/header.js";
-// anyName.test();
-
-// function root () {
-//     console.log("это функция которая находится в основном файле app.js");
-// }
-
-// root();
